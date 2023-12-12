544896b1135782ff51b061b295e9675ea6a951a9
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2023-12-12 20:49:53 +0530

    adding dataTypes code

---
+++ dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all JS code as newer version
+
+//alert("hello javascript"); // we are using nodejs not browser
+
+
+let name = "Bappaditya";
+let age = 30;
+let isLoggedin = true;
+
+//number - 2 to the power of 53
+//bigInt- 
+//string - ""
+//boolean - true or false
+//null-- standalone value
+//undefined- when we cannot defined value but defined value
+//symbol- (it is basically used in ReactJs) unique
+
+//object
+
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
