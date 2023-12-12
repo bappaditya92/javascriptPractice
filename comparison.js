e3cf675d4d2e70d398daa58d26a5a8a03071091c
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2023-12-12 23:22:13 +0530

    adding comparison

---
+++ comparison.js
@@ -0,0 +1,20 @@
+// console.log(2>8);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+//the reason is that an equality check == and comparison >< >= <= work differently
+//comparison convert null to a number, treating it as 0.
+//thats why (3) null>= 0 is true and (1) null>0 is false.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// strict check ===
+
+console.log("2"=== 2);
