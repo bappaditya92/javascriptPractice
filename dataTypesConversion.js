bc78198a975e37edcf32aac1615a20b81fba77b9
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2023-12-12 21:09:05 +0530

    adding dataTpedConversion

---
+++ dataTypesConversion.js
@@ -0,0 +1,29 @@
+let score  = 50;
+// let score  = null;
+// let score  = undefined;
+// let score  = true;
+// let score  = "Bappa";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInString = String(score);
+console.log(typeof valueInString);
+console.log(valueInString);
+
+// "50" => 50;
+// "50abc" => NaN
+// true=> 1; false=>0
+
+let isLoggedIn = "Bappa";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0=> false
+// ""=>false
+// "bappa"=> true
+
+
+let age = 33;
+let stringNumber = String(age);
+console.log(stringNumber);
+console.log(typeof stringNumber);
