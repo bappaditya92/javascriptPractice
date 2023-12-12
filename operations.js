631bffef7ff9aaba658ed6467e3eaa1e79409970
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2023-12-12 21:27:32 +0530

    adding operations in js

---
+++ operations.js
@@ -0,0 +1,27 @@
+//***************Operations*****************
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Bappa";
+let str2 = "ditya";
+let str3 = str1 + str2;
+console.log(str3);
+console.log(1 + "5");
+console.log("1"+ 5+ 3);
+console.log(1+ "5"+ "3");
+
+console.log(+true);
+console.log(+ "");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 1000;
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+//link to stude : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
