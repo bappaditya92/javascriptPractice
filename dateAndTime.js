66f9e2bd075f0733db5707322027504a9f78d354
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2024-01-13 00:48:31 +0530

    adding code to master

---
+++ dateAndTime.js
@@ -0,0 +1,11 @@
+//dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toJSON());3
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2023, 2, 23);
+console.log(myCreatedDate.toDateString());
