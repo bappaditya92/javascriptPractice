bdebd6c2a5d57dbe7888fd25df5171da15804a26
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2024-01-13 00:56:32 +0530

    adding some dateAndTime code

--- dateAndTime.js
+++ dateAndTime.js
@@ -9,3 +9,8 @@ console.log(typeof myDate);
 
 let myCreatedDate = new Date(2023, 2, 23);
 console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+//console/log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
